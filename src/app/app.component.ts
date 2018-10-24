ad9356267176c296371b63191db3d9d0d172c767
Maina Wycliffe <wmmaina7@gmail.com>
2018-10-24 22:47:02 +0300

    added state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { UserRoleState } from './state/user-role.state';
+import { UserRoleAction } from './state/user-role.actions';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngxs-role-based-authorization';
+
+  public userRole: string = null;
+
+  public checkUserRole$ = null;
+
+  constructor(private store: Store) {}
+
+  changeRole(role) {
+    this.store.dispatch(new UserRoleAction(role));
+  }
+
+  ngOnInit() {
+    this.checkUserRole$ = this.store
+      .select(UserRoleState.getCurrentUserRole)
+      .pipe(
+        tap(res => {
+          console.log(res);
+        })
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import { NgxsModule } from '@ngxs/store';
 import { AppComponent } from './app.component';
+import { UserRoleState } from './state/user-role.state';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgxsModule.forRoot([UserRoleState]),
+    NgxsStoragePluginModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
